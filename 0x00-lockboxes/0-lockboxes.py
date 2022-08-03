b9505559287c5e93855e550562216a2eec73a661
Ghassen-bgh <3591@holbertonschool.com>
2022-08-03 12:14:52 +0200

    Lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,9 +7,9 @@ def canUnlockAll(boxes):
 
 	for key in key_list:
 			for box in boxes[key]:
-					if box not in key_list and box < len(boxes):
-							key_list.append(box)
+				if box not in key_list and box < len(boxes):
+					key_list.append(box)
 	for i in range(len(boxes)):
 			if i not in key_list:
-					return False
+				return False
 	return True
