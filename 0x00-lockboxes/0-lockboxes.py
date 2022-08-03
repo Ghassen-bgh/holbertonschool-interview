6e6ccd3749b22e6d59a609acdce3ee88f2d6f74b
Ghassen-bgh <3591@holbertonschool.com>
2022-08-03 12:09:38 +0200

    Lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Locked boxes method
+"""
+def canUnlockAll(boxes):
+    key_list = [0]
+
+    for key in key_list:
+        for box in boxes[key]:
+            if box not in key_list and box < len(boxes):
+                key_list.append(box)
+    for i in range(len(boxes)):
+        if i not in key_list:
+            return False
+    return True
