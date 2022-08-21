664173672d843e3fde9ce60b05bd514c1bd61e8c
Ghassen-bgh <3591@holbertonschool.com>
2022-08-21 16:57:05 +0200

    fix

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -2,15 +2,12 @@
 #include<stdlib.h>
 #include<string.h>
 #include"binary_trees.h"
-
 /**
  * binary_tree_node - Creates a new node
  * @parent: Pointer to the parent node
  * @value: Value to store in the new node
  * Return: Pointer to the new node
  */
-
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node;
