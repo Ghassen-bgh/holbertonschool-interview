fe9331d7a4647c2a80a1a5d417a312dbdbf30b30
Ghassen-bgh <3591@holbertonschool.com>
2022-12-15 16:18:13 +0100

    fix

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -10,29 +10,42 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t i, mid, left, right;
+	size_t i, a, index = 0;
+	int temp;
 
-    if (array == NULL || size == 0)
-        return (-1);
-
-    left = 0;
-    right = size - 1;
-    printf("Searching in array: ");
-    for (i = left; i < right; i++)
-        printf("%d, ", array[i]);
-    printf("%d\n", array[i]);
-
-    if (left == right)
-    {
-        if (array[left] == value)
-            return (left);
-        else
-            return (-1);
-    }
-
-    mid = (left + right) / 2;
-    if (array[mid] >= value)
-        return (advanced_binary(array + left, mid + 1, value));
-    else
-        return (advanced_binary(array + mid + 1, right - mid, value) + mid + 1);
+	if (array == NULL)
+		return (-1);
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		if (i == size - 1)
+			printf("%d\n", array[i]);
+		else
+			printf("%d, ", array[i]);
+	}
+	if (size == 1 && array[0] != value)
+		return (-1);
+	a = (size - 1) / 2;
+	if (array[a] == value)
+	{
+		if (array[a - 1] == value)
+		{
+			index = advanced_binary(array, a + 1, value);
+			return (index);
+		}
+		return (a);
+	}
+	if (array[a] > value)
+	{
+		index = advanced_binary(array, a + 1, value);
+		return (index);
+	}
+	if (array[a] < value)
+	{
+		temp = advanced_binary(array + a + 1, size - a - 1, value);
+		if (temp == -1)
+			return (-1);
+		return (a + 1 + temp);
+	}
+	return (-1);
 }
