d98f77706195165da678a2250c6b27c508be6561
Ghassen-bgh <3591@holbertonstudents.com>
2023-01-11 11:10:13 +0100

    HEADER FILE

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+int _putchar(char c);
+int mul(int num1, int num2);
+int is_valid_number(char *num);
+int main(int argc, char **argv);
+
+#endif
