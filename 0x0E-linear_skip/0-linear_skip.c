6e3c56df62a6a6df1823ea2d05ac3a5f4fe64668
Ghassen-bgh <3591@holbertonstudents.com>
2023-01-27 13:34:41 +0100

    linear skip fix

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,46 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "search.h"
+
 /**
- * linear_skip - searches for a value in a sorted skip list of integers
- * @list: pointer to the head of the skip list to search in
- * @value: value to search for
- * Return: pointer on the first node where value is located
+ * linear_skip - optimize the time complexity of a search
+ * in a singly linked list
+ *
+ * @list: List
+ * @value: value
+ *
+ * Return: Always EXIT_SUCCESS
  */
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *tmp = list;
+	skiplist_t *tmp = NULL;
 
 	if (!list)
 		return (NULL);
+
+	tmp = list;
 	while (tmp->express)
 	{
-		printf("Value checked at index [%lu] = [%d]/n",
-		       tmp->express->index, tmp->express->n);
+		printf("Value checked at index [%lu] = [%d]\n",
+		tmp->express->index, tmp->express->n);
 		if (tmp->express->n >= value)
 		{
-			printf("Value found between indexes [%lu] and [%lu]/n",
-			       tmp->index, tmp->express->index);
+			printf("Value found between indexes [%lu] and [%lu]\n",
+					tmp->index, tmp->express->index);
 			break;
 		}
-		tmp = tmp->express;
+	tmp = tmp->express;
 	}
+
 	if (!tmp->express)
 	{
-		for (list = tmp; list->next; list = list->next)
-			;
-		printf("Value found between indexes [%lu] and [%lu]/n",
-		       tmp->index, list->index);
+		list = tmp;
+		while (list->next)
+			list = list->next;
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				tmp->index, list->index);
 	}
-	for (list = tmp; list != tmp->express; list = list->next)
+
+	list = tmp;
+
+	while (list != tmp->express)
 	{
-		printf("Value checked at index [%lu] = [%d]/n",
-		       list->index, list->n);
+		printf("Value checked at index [%lu] = [%d]\n",
+		list->index, list->n);
 		if (list->n == value)
-		{
-			break;
-		}
+			return (list);
+		list = list->next;
 	}
-	if (list == tmp->express)
-		return (NULL);
-	return (list);
+	return (NULL);
 }
