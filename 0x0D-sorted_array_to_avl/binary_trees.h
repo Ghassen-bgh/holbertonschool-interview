db223f1ad1af49fe903e25125382689795a79ca3
Ghassen-bgh <3591@holbertonstudents.com>
2023-02-20 13:38:17 +0100

    header file

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -1,6 +1,5 @@
-#ifndef binary_trees_h
-#define binary_trees_h
-
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
 #include <stdio.h>
@@ -13,17 +12,16 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
-
 typedef struct binary_tree_s avl_t;
 
 void binary_tree_print(const binary_tree_t *);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 
-#endif /* binary_trees_h */
+#endif
