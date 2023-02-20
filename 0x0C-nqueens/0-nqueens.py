3c95f6259fb477c65566465d3f2f26f0d71d9dfd
Ghassen-bgh <3591@holbertonstudents.com>
2023-02-20 13:05:40 +0100

    nqueens fix

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""N queens puzzle"""
+"""
+The N queens puzzle
+"""
 
 
 import sys
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    sys.exit(1)
+    exit(1)
 try:
     int(sys.argv[1])
 except Exception:
     print("N must be a number")
-    sys.exit(1)
+    exit(1)
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    sys.exit(1)
+    exit(1)
 if not isinstance(int(sys.argv[1]), int):
     print("N must be a number")
-    sys.exit(1)
+    exit(1)
 
 n = int(sys.argv[1])
 
@@ -32,18 +34,17 @@ def get_candidates(state, n):
 
     position = len(state)
     candidates = set(range(n))
-    for i, queen in enumerate(state):
-        candidates.discard(queen)
-        diff = position - i
-        candidates.discard(queen - diff)
-        candidates.discard(queen + diff)
+    for row, col in enumerate(state):
+        candidates.discard(col)
+        dist = position - row
+        candidates.discard(col + dist)
+        candidates.discard(col - dist)
     return candidates
 
 
 def search(state, solutions, n):
     if is_valid_state(state, n):
         state_string = state_to_string(state)
-        state.append(candidate)
         solutions.append(state_string)
         return
 
