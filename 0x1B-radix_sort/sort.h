063b4a3712763c53a72b9fab3daaa0b121db49b7
Ghassen-bgh <3591@holbertonstudents.com>
2023-03-17 15:26:15 +0100

    header file

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+int get_max(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+
+
+#endif
