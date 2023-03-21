858c1e172dd9d91cb4b3e38d87212d29b05909f2
Ghassen-bgh <3591@holbertonstudents.com>
2023-03-21 11:38:02 +0100

    making change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,11 +3,23 @@
 Make Change Module
 """
 def makeChange(coins, total):
-	if total == 0:
+	"""
+	Make Change
+	"""
+	if total <= 0:
 		return 0
-	minCoins = total
-	for i in [c for c in coins if c <= total]:
-		numCoins = 1 + makeChange(coins, total - i)
-		if numCoins < minCoins:
-			minCoins = numCoins
-	return minCoins
+	if total in coins:
+		return 1
+	if len(coins) == 0:
+		return -1
+	M = [0]*(total + 1)
+
+	for j in range(1, total + 1):
+		M[j] = total + 1
+
+	for i in range(1, total + 1):
+		for coin in coins:
+			if i - coins >= 0:
+				M[i] = min(M[i], M[i - coin] + 1)
+	return M[total] if M[total] != total + 1 else -1
+
