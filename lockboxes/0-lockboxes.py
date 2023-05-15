c322878eac4845e107fb1a550ff790ca8ede5fd3
Ghassen-bgh <91068824+Ghassen-bgh@users.noreply.github.com>
2023-05-15 12:12:38 +0100

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,17 +5,19 @@ Locked boxes method
 
 
 def canUnlockAll(boxes):
+    l=[]
+    X=len(boxes)
+    for i in boxes:
+        if len(i)==0 and i is not boxes[X-1]: #### if the empty list is not the last list in the boxes (in the middle) [case of third]
+            return False
 
-    numberofBoxes = len(boxes)
-    unlocked= [0]
-    numberofUnlockedBoxes = 1
+        for j in i:
+            l.append(j)  #creation  de la  list: change the list of list into a simple list
 
-    for box in unlocked:
-      
-        for key in boxes[box]:
 
-             if key < numberofBoxes and key not in unlocked:
-                 unlocked.append(key)
-                 numberofUnlockedBoxes += 1
-
-    return numberofBoxes == numberofUnlockedBoxes
+    #print(l) affichage
+    for index, keys in enumerate(boxes):
+        if index in l or index<X-1:
+            return True
+        else:
+            return False
