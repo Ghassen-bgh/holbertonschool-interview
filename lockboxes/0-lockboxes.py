d92d2b869f7d28fe1b07df53795845a3e1097c9e
Ghassen-bgh <91068824+Ghassen-bgh@users.noreply.github.com>
2023-05-15 11:19:36 +0100

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -7,15 +7,15 @@ Locked boxes method
 def canUnlockAll(boxes):
 
     numberofBoxes = len(boxes)
-    unlocked = [0]
+    unlocked= [0]
     numberofUnlockedBoxes = 1
 
     for box in unlocked:
-
+      
         for key in boxes[box]:
 
-            if key <= numberofBoxes and key not in unlocked:
-                unlocked.append(key)
-                numberofUnlockedBoxes += 1
+             if key < numberofBoxes and key not in unlocked:
+                 unlocked.append(key)
+                 numberofUnlockedBoxes += 1
 
     return numberofBoxes == numberofUnlockedBoxes
